c2d4b300cf91b9a2a652f5246295fea9cb47f9e1
samhernandezc24 <samd.hernandezc24@gmail.com>
2023-11-02 12:33:16 -0600

    feat: Agregar nuevos Tasks para los métodos correspondientes del BelvoService

--- API.Belvo/Services/BelvoService.cs
+++ API.Belvo/Services/BelvoService.cs
@@ -7,8 +7,7 @@ namespace API.Belvo.Services
     {
         static string belvoApiUrl = "https://sandbox.belvo.com/";
         public readonly Context _context;
-
-        // public static IEnumerable<dynamic> lstHeaders = new List<dynamic>().Append(new { name = "Authorization", value = "Basic N2E0NTUyNjUtYjVmNS00NmJkLTllNjUtYmVhYTg4NTg1MWU1OkRAeWxtYSNFaHpyVDM0bzUxVXZ6czJqQlVpQzhvcUEwTGxRZUN6bHlfWFU5alI4RkE0OV9kMVB0ZXJVOFVHTEM=" });
+        public static IEnumerable<dynamic> lstHeaders = new List<dynamic>().Append(new { name = "Authorization", value = "Basic N2E0NTUyNjUtYjVmNS00NmJkLTllNjUtYmVhYTg4NTg1MWU1OkRAeWxtYSNFaHpyVDM0bzUxVXZ6czJqQlVpQzhvcUEwTGxRZUN6bHlfWFU5alI4RkE0OV9kMVB0ZXJVOFVHTEM=" });
 
         public BelvoService (Context context)
         {
@@ -17,29 +16,84 @@ namespace API.Belvo.Services
 
         // A
         #region ACCOUNTS
-
-        public static async Task<dynamic> GetAccountsList(List<dynamic> headers)
+        public static async Task<dynamic> GetAccountsList()
         {
             try
             {
-                return await WebServiceManager.Get(belvoApiUrl + "api/accounts", headers: headers);
+                return await WebServiceManager.Get(belvoApiUrl + "api/accounts", lstHeaders);
             }
             catch (Exception e)
             {
-                // Manejar la excepción.
                 throw new ArgumentException(e.Message + e.StackTrace);
             }
         }
 
-        public static async Task<dynamic> AccountsCreate(dynamic parameters)
+        public static async Task<dynamic> AccountsStore(dynamic parameters)
         {
-            var result = await WebServiceManager.Post(belvoApiUrl + "api/accounts", parameters);
-            return result;
+            try
+            {
+                return await WebServiceManager.Post(belvoApiUrl + "api/accounts", parameters);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException(e.Message + e.StackTrace);
+            }
+        }
+        #endregion
+
+        // I
+        #region INSTITUTIONS
+        public static async Task<dynamic> GetInstitutionsList()
+        {
+            try
+            {
+                return await WebServiceManager.Get(belvoApiUrl + "api/institutions", lstHeaders);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException(e.Message + e.StackTrace);
+            }
         }
 
+        public static async Task<dynamic> GetInstitutionsDetails(string id)
+        {
+            try
+            {
+                return await WebServiceManager.Get(belvoApiUrl + "api/institutions/" + id, lstHeaders);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException(e.Message + e.StackTrace);
+            }
+        }
         #endregion
 
         // L
+        #region LINKS
+        public static async Task<dynamic> GetLinksList()
+        {
+            try
+            {
+                return await WebServiceManager.Get(belvoApiUrl + "api/links", lstHeaders);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException(e.Message + e.StackTrace);
+            }
+        }
+
+        public static async Task<dynamic> LinksNew(dynamic objParameters)
+        {
+            try
+            {
+                return await WebServiceManager.Post(belvoApiUrl + "api/links", lstHeaders, objParameters);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException(e.Message + e.StackTrace);
+            }
+        }
+        #endregion
 
         // T
     }
