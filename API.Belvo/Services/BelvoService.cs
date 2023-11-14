b2631791a0575bdf48bb40f04b50c32eead969df
Sam Hernandez <samd.hernandezc24@gmail.com>
2023-11-14 02:09:12 -0600

    feat: Agregar los metodos para BelvoService

--- API.Belvo/Services/BelvoService.cs
+++ API.Belvo/Services/BelvoService.cs
@@ -1,5 +1,8 @@
 ﻿using API.Belvo.Libraries;
+using API.Belvo.Models;
 using API.Belvo.Persistence;
+using API.Belvo.ViewModels;
+using Newtonsoft.Json;
 
 namespace API.Belvo.Services
 {
@@ -19,7 +22,7 @@ namespace API.Belvo.Services
         #region ACCOUNTS
         /*
         |--------------------------------------------------------------------------
-        | Obtiene una "Institucion" [GET]
+        | [GET] Lista de todas las "Cuentas"
         |--------------------------------------------------------------------------
         |
         | Se obtiene una lista paginada de todas las "Cuentas" existentes en
@@ -36,11 +39,119 @@ namespace API.Belvo.Services
             }
             catch (HttpRequestException httpEx)
             {
-                throw new ArgumentException("Error en la petición HTTP: " + httpEx.Message, httpEx);
+                throw new ArgumentException("Ha ocurrido un error en la petición HTTP: " + httpEx.Message, httpEx);
             }
             catch (Exception ex)
             {
-                throw new ArgumentException("Error inesperado: " + ex.Message, ex);
+                throw new ArgumentException("Ha ocurrido un error inesperado: " + ex.Message, ex);
+            }
+        }
+
+        /*
+        |--------------------------------------------------------------------------
+        | [POST] Recupera las cuentas de un "Link"
+        |--------------------------------------------------------------------------
+        |
+        | Se recupera las "Cuentas" de un "Link" existente.
+        |
+        */
+        public static async Task<CuentaListResult> RetrieveAccountsForLink(dynamic objBodyParams)
+        {
+            try
+            {
+                var result = await WebServiceManager.Post(belvoApiUrl + "api/accounts", lstHeaders, objBodyParams);
+                
+                if (!result.isSuccessful) { throw new ArgumentException($"{result.statusCode} - No se pudieron obtener las cuentas."); }
+
+                CuentaListResult accountData = JsonConvert.DeserializeObject<CuentaListResult>(result.body);
+
+                return accountData;
+
+            }
+            catch (HttpRequestException httpEx)
+            {
+                throw new ArgumentException("Ha ocurrido un error en la petición HTTP: " + httpEx.Message, httpEx);
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Ha ocurrido un error inesperado: " + ex.Message, ex);
+            }
+        }
+
+        /*
+        |--------------------------------------------------------------------------
+        | [PATCH] Completa una solicitud de "Cuentas"
+        |--------------------------------------------------------------------------
+        |
+        | Se utiliza para reanudar una sesión de recuperación de "Cuentas" que se
+        | detuvo porque la institución requería un token MFA.
+        |
+        */
+        public static async Task<dynamic> CompleteAccountsRequest(dynamic objBodyParams)
+        {
+            try
+            {
+                var result = await WebServiceManager.Patch(belvoApiUrl + "api/accounts", lstHeaders, objBodyParams);
+                return result;
+            }
+            catch (HttpRequestException httpEx)
+            {
+                throw new ArgumentException("Ha ocurrido un error en la petición HTTP: " + httpEx.Message, httpEx);
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Ha ocurrido un error inesperado: " + ex.Message, ex);
+            }
+        }
+
+        /*
+        |--------------------------------------------------------------------------
+        | [GET] Obtiene los datos de una "Cuenta"
+        |--------------------------------------------------------------------------
+        |
+        | Obtiene los detalles de una "Cuenta" específica.
+        |
+        */
+        public static async Task<dynamic> AccountsDetails(string id)
+        {
+            try
+            {
+                var result = await WebServiceManager.Get(belvoApiUrl + "api/accounts/" + id, lstHeaders);
+                return result;
+            }
+            catch (HttpRequestException httpEx)
+            {
+                throw new ArgumentException("Ha ocurrido un error en la petición HTTP: " + httpEx.Message, httpEx);
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Ha ocurrido un error inesperado: " + ex.Message, ex);
+            }
+        }
+
+        /*
+        |--------------------------------------------------------------------------
+        | [DELETE] Elimina una "Cuenta"
+        |--------------------------------------------------------------------------
+        |
+        | Elimina una "Cuenta" específica y todas las transacciones asociadas, así
+        | como propietarios, de la cuenta Belvo.
+        |
+        */
+        public static async Task<dynamic> AccountsDelete(string id)
+        {
+            try
+            {
+                var result = await WebServiceManager.Delete(belvoApiUrl + "api/accounts/" + id, lstHeaders);
+                return result;
+            }
+            catch (HttpRequestException httpEx)
+            {
+                throw new ArgumentException("Ha ocurrido un error en la petición HTTP: " + httpEx.Message, httpEx);
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Ha ocurrido un error inesperado: " + ex.Message, ex);
             }
         }
         #endregion
